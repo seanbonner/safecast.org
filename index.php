1c7358f7b871fabde4a99a9cae91a075df796887
Sean Bonner <sean@seanbonner.com>
2011-08-26 10:59:20 -0700

    full data text edit

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ $tmpl->place('header');
 						  	<!--<p style="padding-top: 10px;"><img src="/images/gridmap_grey.png" class="pinImage" height="18" width="18"/> <?php echo $translations->gridMapLegend1; ?></p> 
 						      <p style="padding-top: 10px;"><img src="/images/gridmap_green.png" class="pinImage" height="18" width="18"/> <?php echo $translations->gridMapLegend2; ?></p> -->
 							  <p><?php echo $translations->gridMapBody; ?> <a href="<?php echo $translations->driveListHref; ?>"><?php echo $translations->gridMapBodyLink; ?></a></p> 
-							  <p><br /><br /><div class="redButton"><a href="/fusion">Interact with full data set<br />全データセットを見る</a></div><br /></p>
+							  <p><br /><br /><div class="redButton"><a href="/fusion">All Safecast Data<br />全データセットを見る</a></div><br /></p>
   						      <p><div class="redButton"><a href="<?php echo $translations->driveListHref; ?>">Individual drive maps<br />各ドライブマップ</a></div></p>
 
 						      <div id="donateBox" align="center" style="margin-top: 180px;">Want to help us collect more data?<br />データ収集支援のため寄付をする<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
